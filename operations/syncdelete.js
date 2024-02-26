724a115beffac560d6dfe363b879b8934d7a3b69
Xintong Zhang <zhang.xintong@northeastern.edu>
2024-02-25 19:42:03 -0500

    update

--- operations/syncdelete.js
+++ operations/syncdelete.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
+
 fs.readFile("./operations/data.txt", (err, data) => {
   if (err) console.log(err.message);
   else console.log(data);
+  // Move the unlink operation inside the callback
+  fs.unlink("./operations/data.txt", (err) => {
+    if (err) console.log(err.message);
+    else console.log("File deleted successfully");
+  });
 });
-fs.unlinkSync("./operations/data.txt");		// delete the file
-
